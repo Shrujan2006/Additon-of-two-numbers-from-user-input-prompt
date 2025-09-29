fd395cd58142c76f3713ccd6724eb185c83064c4
SHRUJAN S <shrujans17@gmail.com>
2025-09-29 20:55:01 +0530

    Create Addition.java
    This program is use to add two integer vakes that the user inputs.
    This is in java language.

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class add{
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int b = a.nextInt();
+        System.out.println("Enter the second number: ");
+        int n = a.nextInt();
+        System.out.println("The addition of " + b +  " + " + n + " is " + (n + b));
+    }
+}
+

8760c3ecc8848342a84f18060cf992b20edfce86
SHRUJAN S <shrujans17@gmail.com>
2025-09-30 20:31:47 +0530

    Add files via upload
    This program is about calculating the sum and the average of N numbers, the user is needed to specify the size of the array(N), then required to enter Number of elements to get their sum and the average. Then finally prints Thankyou.

---
+++ Array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import javax.sound.sampled.SourceDataLine;
+class Array{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sum=0,size;
+        float avg;
+
+        System.out.println("Enter the size of the array");  
+        size=sc.nextInt();
+        int m[]=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter  the "+(i+1)+"st number :");
+            m[i]=sc.nextInt();
+            sum=sum+m[i];
+        }
+        System.out.println("The sum of all the  numbers is :"+sum);
+            avg=(float)sum/size;
+        System.out.println("The average of the 3 numbers is: "+avg);
+        System.out.println("Thank you....");
+
+    }
+
+
+}
+
